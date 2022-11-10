780c9ff9c3477017c6c9b596f0483a2d1b9ac2c3
Juhyeon Lee <gogori6565@naver.com>
2022-11-10 16:10:20 +0900

    10-Event

---
+++ PROJECT221109/KeyCodeEx.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class KeyCodeEx extends JFrame {
+	JPanel contentPane = new JPanel();
+	private JLabel la = new JLabel();
+
+	public KeyCodeEx() {
+		setTitle("Key Code 예제 : F1키:초록색, % 키 노란색");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(contentPane);
+		Container c = getContentPane();
+		c.addKeyListener(new MyKeyListener());
+		c.add(la);
+		setSize(300, 150);
+		setVisible(true);
+		c.setFocusable(true);
+		c.requestFocus();
+	}
+
+	class MyKeyListener extends KeyAdapter {
+		public void keyPressed(KeyEvent e) {
+			la.setText(e.getKeyText(e.getKeyCode()));
+			if (e.getKeyChar() == '%')
+				contentPane.setBackground(Color.YELLOW);
+			else if (e.getKeyCode() == KeyEvent.VK_F1)
+				contentPane.setBackground(Color.GREEN);
+		}
+	}
+
+	public static void main(String[] args) {
+		new KeyCodeEx();
+	}
+}
